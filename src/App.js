2683377a0e9f3de9ca961bcf47164901f358cd3e
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-11-19 19:43:53 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+
+    useEffect(() => {
+      const handleResize = () =>
+        setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+
+      window.addEventListener('resize', handleResize);
+
+      handleResize();
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+    return windowSize;
+  };
+
+  const { width, height } = useWindowSize();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <h3>This window size is</h3>
+        <h1>({width} x {height})</h1>
+      </div>
     </div>
   );
 }
